6916dbc2dd3137c70bfc706a479a9082b5c5af12
Davidjuhasz05 <juhasz.david.2005@gmail.com>
2025-12-09 16:24:41 +0100

    feat: Create api client interfaces

--- src/main/java/com/codecool/solarwatch/client/GeoClient.java
+++ src/main/java/com/codecool/solarwatch/client/GeoClient.java
@@ -1,7 +1,19 @@
 package com.codecool.solarwatch.client;
 
+import com.codecool.solarwatch.client.response.CityCoordinates;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
-@HttpExchange
+import java.util.List;
+
+@HttpExchange(url = "https://api.openweathermap.org/geo/1.0/direct")
 public interface GeoClient {
+
+  @GetExchange("/")
+  List<CityCoordinates> fetchCoordinates(
+          @RequestParam("q") String city,
+          @RequestParam("appid") String apiKey
+  );
+
 }
