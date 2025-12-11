34e861dc8b395adb5061cc74768ce7b06b584a52
Davidjuhasz05 <juhasz.david.2005@gmail.com>
2025-12-11 02:01:02 +0100

    chore: Add .env.example and remove unused import

--- src/test/java/com/codecool/solarwatch/controller/SolarEventsControllerTest.java
+++ src/test/java/com/codecool/solarwatch/controller/SolarEventsControllerTest.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
