e359423a1a5413d7e682f3373f222f66ddec3f9a
Davidjuhasz05 <juhasz.david.2005@gmail.com>
2026-01-16 16:32:28 +0100

    feat: Add User and Role entities

---
+++ src/main/java/com/codecool/solarwatch/model/user/Role.java
@@ -0,0 +1,22 @@
+package com.codecool.solarwatch.model.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, unique = true)
+  private RoleType roleType;
+
+}
---
+++ src/main/java/com/codecool/solarwatch/model/user/UserEntity.java
@@ -0,0 +1,31 @@
+package com.codecool.solarwatch.model.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+  private Set<Role> roles;
+
+}
