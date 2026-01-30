7beab1a6859414a3a9e3a833fbb8561547da8c65
Davidjuhasz05 <juhasz.david.2005@gmail.com>
2026-01-30 21:18:29 +0100

    feat: Add error handling for existing user, refactor ApiError

--- src/main/java/com/codecool/solarwatch/exception/ApiError.java
+++ src/main/java/com/codecool/solarwatch/exception/ApiError.java
@@ -1,11 +1,13 @@
 package com.codecool.solarwatch.exception;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
 public class ApiError {
   private LocalDateTime timestamp;
   private int status;
